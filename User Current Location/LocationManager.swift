6d299c7d67f31b743bae2507b29fefa2a22d36fc
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-11-29 16:49:52 -0500

    Initial Commit

---
+++ User Current Location/LocationManager.swift
@@ -0,0 +1,33 @@
+//
+//  LocationManager.swift
+//  User Current Location
+//
+//  Created by Franco Rodrigues on 11/29/21.
+//
+
+
+import CoreLocation
+import Foundation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    
+    let manager = CLLocationManager()
+    
+    var completion: ((CLLocation) -> Void)?
+    
+    public func getUserLocation(completion: @escaping ((CLLocation) -> Void))  {
+        self.completion = completion
+        manager.requestWhenInUseAuthorization()
+        manager.delegate = self
+        manager.startUpdatingLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        completion?(location)
+        manager.stopUpdatingLocation()
+    }
+    
+}
