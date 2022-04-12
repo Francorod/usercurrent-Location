386a1b6bc1c2009da7f6bc89857f289d4792cd73
Franco Rod <francorodios@gmail.com>
2022-04-11 20:06:36 -0400

    User current localization

--- User Current Location/ViewController.swift
+++ User Current Location/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
             guard let strongSelf = self else {
                 return
             }
+            //Pin method
             let pin = MKPointAnnotation()
             pin.coordinate = location.coordinate
             strongSelf.map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan (latitudeDelta: 0.7, longitudeDelta: 0.7)),
